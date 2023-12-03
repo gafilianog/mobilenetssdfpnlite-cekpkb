1d476c3651f520dee737109b2aafa644e41fd653
Gafiliano Gapili <gafiliano@gmail.com>
2023-12-03 22:21:05 +0700

    feat(detect): add built in cam detector
    Signed-off-by: Gafiliano Gapili <gafiliano@gmail.com>

--- app/src/main/java/dev/gafilianog/cekpkb/MainActivity.kt
+++ app/src/main/java/dev/gafilianog/cekpkb/MainActivity.kt
@@ -1,16 +1,151 @@
 package dev.gafilianog.cekpkb
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.lifecycle.lifecycleScope
 import dev.gafilianog.cekpkb.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.vision.detector.Detection
+import org.tensorflow.lite.task.vision.detector.ObjectDetector
+import java.io.File
+import java.io.IOException
+import java.lang.StringBuilder
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.TreeMap
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var currentPhotoPath: String
+    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            setViewAndDetect(getCapturedImage())
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnTakePicture.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_take_picture -> {
+                try {
+                    takePictureIntent()
+                } catch (e: ActivityNotFoundException) {
+                    Log.e(TAG, e.message.toString())
+                }
+            }
+        }
+    }
+
+    private fun takePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (e: IOException) {
+                    Log.e(TAG, e.message.toString())
+                    null
+                }
+
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "dev.gafilianog.cekpkb.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    resultLauncher.launch(takePictureIntent)
+                }
+            }
+        }
+    }
+
+    private fun createImageFile(): File {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        ).apply {
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun setViewAndDetect(bitmap: Bitmap) {
+        lifecycleScope.launch(Dispatchers.Default) { runObjectDetection(bitmap) }
+    }
+
+    private fun getCapturedImage(): Bitmap {
+        val bmOptions = BitmapFactory.Options().apply {
+            inJustDecodeBounds = true
+            BitmapFactory.decodeFile(currentPhotoPath, this)
+            inJustDecodeBounds = false
+            inMutable = true
+        }
+
+        return BitmapFactory.decodeFile(currentPhotoPath, bmOptions)
+    }
+
+    private fun runObjectDetection(bitmap: Bitmap) {
+        val image = TensorImage.fromBitmap(bitmap)
+
+        val options = ObjectDetector.ObjectDetectorOptions.builder()
+            .setMaxResults(10)
+            .setScoreThreshold(0.4f)
+            .build()
+
+        val detector = ObjectDetector.createFromFileAndOptions(
+            this,
+            "platenum_quant_metadata.tflite",
+            options
+        )
+
+        val results = detector.detect(image)
+
+        val licenseNumber = getDetectionLabel(results)
+
+        runOnUiThread {
+            binding.tvLicenseNumber.text = licenseNumber
+        }
+    }
+
+    private fun getDetectionLabel(results: List<Detection>): String {
+        val licenseNumber = StringBuilder()
+        val classMap: MutableMap<Float, String> = HashMap()
+
+        results.forEach { obj ->
+            obj.categories.forEach { category -> classMap[obj.boundingBox.left] = category.label }
+        }
+
+        val sortedClassMap = TreeMap(classMap)
+        sortedClassMap.values.forEach {char -> licenseNumber.append(char)}
+
+        return licenseNumber.toString()
+    }
+
+    companion object {
+        const val TAG = "TFLite - ODT"
     }
 }
